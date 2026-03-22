fef502dc53965610c21ecea6319440f890771e21
Humberto Fernandes <humbertofernandes08@hotmail.com>
2026-03-21 22:00:26 -0300

    start project

---
+++ src/main/java/com/fiap/hackaton/sus_helper/SusHelperApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.hackaton.sus_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SusHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/hackaton/sus_helper/SusHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.hackaton.sus_helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SusHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
