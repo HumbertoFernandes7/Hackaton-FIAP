7d3c897b0dfa751bf5a834b9c2a58c825e75e0fc
Humberto Fernandes <humbertofernandes08@hotmail.com>
2026-03-22 23:47:49 -0300

    feat(WaitingTime) add repository

---
+++ src/main/java/com/fiap/hackaton/sus/helper/repositories/WaitingTimeRepository.java
@@ -0,0 +1,14 @@
+package com.fiap.hackaton.sus.helper.repositories;
+
+import com.fiap.hackaton.sus.helper.entities.HealthUnitEntity;
+import com.fiap.hackaton.sus.helper.entities.WaitingTimeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface WaitingTimeRepository extends JpaRepository<WaitingTimeEntity, Long> {
+
+    boolean existsByHealthUnitId_Id(UUID healthUnitId);
+
+    WaitingTimeEntity findByHealthUnitId(HealthUnitEntity healthUnitId);
+}
