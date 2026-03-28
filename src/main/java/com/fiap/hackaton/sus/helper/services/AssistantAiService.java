237d53a9e86f1f6ec816c841e3793068cbf502cb
Humberto Fernandes <humbertofernandes08@hotmail.com>
2026-03-28 17:40:14 -0300

    feat(ai-assistant) Improved AI prompt and added a tool to check wait times based on color coding

--- src/main/java/com/fiap/hackaton/sus/helper/services/AssistantAiService.java
+++ src/main/java/com/fiap/hackaton/sus/helper/services/AssistantAiService.java
@@ -9,16 +9,23 @@ import dev.langchain4j.service.spring.AiService;
 public interface AssistantAiService {
 
     @SystemMessage("""
-            
             FUNÇÃO:
             Você é um assistente de um gerenciamento inteligente de paciente para tratamento no sus.
             Você recebera a localização do usuário e deve buscar por meio das "tools" as unidades mais próximas e com menos tempo de atendimento.
             Você deve apenas analisar o problema do usuário e direcionar ele para o tipo de unidade de saúde necessária, sendo ela UBS, UPA e Hospital.
+            Você deve classificar o relato do usuário por uma cor "blue", "green", "yellow", "red".
+            Você deve analisar qual unidade encontrada tem o menor tempo e para o atendimento do usuário.
             
             IMPORTANTE:
             - Utilize apenas unidades cadastradas no sistema e retornadas pelas ferramentas "tools"
-            - Não invente unidade diferentes e não sugira tratamentos, seu dever é apenas identificar pra qual unidade o usuário deve ir com base no problema, tempo de atendimento e localização
-            - Voce deve explicar o motivo da sua tomada de decisão, listar o tempo de atendimento da sua escolha
+            - Utilize apenas o tempo de espera retornada pelo sistema usando as ferramentas "tools"
+            - Não invente unidade diferentes e não sugira tratamentos, seu dever é apenas identificar o problema do usuário, classificar ele com uma cor e direcionar ele pra qual unidade o usuário deve ir com base no problema, tempo de atendimento e localização
+            - Voce deve explicar o motivo da sua tomada de decisão, listar o tempo de atendimento da sua escolha.
+            - Blue são casos mais leves e red os casos de maior emergência.
+            
+            RETORNO:
+            - Retorne na mensagem, todas as unidades recebidas pela ferramente e qual você indica.
+            - Retorne a cor em que você deu ao usuário e por que tal cor.
             """)
     Result<String> handleRequest(@UserMessage String userMessage);
 
--- src/main/java/com/fiap/hackaton/sus/helper/tools/AssistantAiTools.java
+++ src/main/java/com/fiap/hackaton/sus/helper/tools/AssistantAiTools.java
@@ -2,6 +2,7 @@ package com.fiap.hackaton.sus.helper.tools;
 
 
 import com.fiap.hackaton.sus.helper.entities.HealthUnitEntity;
+import com.fiap.hackaton.sus.helper.entities.WaitingTimeEntity;
 import com.fiap.hackaton.sus.helper.services.HealthUnitService;
 import com.fiap.hackaton.sus.helper.services.WaitingTimeService;
 import dev.langchain4j.agent.tool.Tool;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
@@ -17,13 +19,13 @@ public class AssistantAiTools {
     private final HealthUnitService healthUnitService;
     private final WaitingTimeService waitingTimeService;
 
-    @Tool("Encontra a as unidades mais próximas")
+    @Tool("Encontrar as unidades mais próximas")
     public List<HealthUnitEntity> getBestByLocation(String location){
         return healthUnitService.getBestByLocation(location);
     }
 
-    /*@Tool("Retorna o tempo de espera")
-    public List<WaitingTimeEntity> getWaitingTime(List<HealthUnitEntity> healthUnits){
-        return null;
-    }*/
+    @Tool("Encontrar o tempo de espera das unidades")
+    public List<WaitingTimeEntity> getWaitingTimesByHealthUnitIds(List<UUID> healthUnitIds){
+        return waitingTimeService.findWaitingTimesByHealthUnitIds(healthUnitIds);
+    }
 }
